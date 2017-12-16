eb7abb271b06984a65e5ef9fa80a55a6a6c58920
bobo520 <986119012@qq.com>
2017-12-16 18:30:14 +0800

    Update Demo5.java

--- src/com/day1/Demo5.java
+++ src/com/day1/Demo5.java
@@ -2,9 +2,12 @@ package com.day1;
 
 /**
  * Created by 谭杰文 on 2017/12/16.
+ 我真机智!
  */
 public class Demo5 {
     public static void main(String[] args) {
+        
+    
         System.out.println("哈哈!我是通过idea上传的!");
     }
 }

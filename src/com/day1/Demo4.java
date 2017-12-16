9a1e5b611fa4e8d398e6d4be2a4c289a3ec683ff
Bobo <986119012@qq.com>
2017-12-16 18:17:31 +0800

    Demo4提交

---
+++ src/com/day1/Demo4.java
@@ -0,0 +1,10 @@
+package com.day1;
+
+/**
+ * Created by 谭杰文 on 2017/12/16.
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        System.out.println("我才是真正新来的!");
+    }
+}

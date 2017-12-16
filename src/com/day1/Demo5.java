bc59294c8612c70cfc91e9962c2fda5850e93ea9
Bobo <986119012@qq.com>
2017-12-16 18:24:58 +0800

    通过idea上传

---
+++ src/com/day1/Demo5.java
@@ -0,0 +1,10 @@
+package com.day1;
+
+/**
+ * Created by 谭杰文 on 2017/12/16.
+ */
+public class Demo5 {
+    public static void main(String[] args) {
+        System.out.println("哈哈!我是通过idea上传的!");
+    }
+}

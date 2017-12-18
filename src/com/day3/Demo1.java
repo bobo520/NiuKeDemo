b2d9cc90a32773d93ecd89228a1593943027c157
Bobo <986119012@qq.com>
2017-12-18 11:48:29 +0800

    finally执行

---
+++ src/com/day3/Demo1.java
@@ -0,0 +1,28 @@
+package com.day3;
+
+/**
+ * Created by 谭杰文 on 2017/12/18.
+ */
+public class Demo1 {
+    public static void main(String args[]){
+        int num = 10;
+        System.out.println(test(num));
+    }
+    public static int test(int b){
+        try {
+            b += 10;
+            return b;
+        }
+        catch(RuntimeException e)
+        {
+            }
+        catch(Exception e2)
+        {
+        }
+        finally
+        {
+            b += 10;
+            return b;
+        }
+        }
+}
